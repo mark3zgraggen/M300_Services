7f57196c4da8bdca4bf223e70587b57b53a64e75
mark3zgraggen <mark.zgraggen@edu.tbz.ch>
2022-03-23 18:59:28 +0100

    edit bootstrap file

--- lb2/bootstrap.sh
+++ lb2/bootstrap.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
-
+ 
 DBHOST=localhost
-DBNAME=dbname
-DBUSER=dbuser
-DBPASSWD=userpass
+DBNAME=m300
+DBUSER=mzgraggen
+DBPASSWD=dinitante
 
 apt-get update
-apt-get install vim curl build-essential python-software-properties git
 
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
@@ -16,8 +14,6 @@ debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBP
 debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
 
-# install mysql and admin interface
-
 apt-get -y install mysql-server phpmyadmin
 
 mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
@@ -25,22 +21,8 @@ mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' 
 
 cd /vagrant
 
-# update mysql conf file to allow remote access to the db
-
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 sudo service mysql restart
-
-# setup phpmyadmin
-
-apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-gettext a2enmod rewrite
-
-sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-
-rm -rf /var/www/html
-ln -fs /vagrant/public /var/www/html
-
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
 
 service apache2 restart
